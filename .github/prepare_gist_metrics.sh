f202b17f00a161c6115858b4293cd03f2cd228aa
Vadim Dor <V.Dorez@mail.ru>
2024-03-18 22:24:03 +0300

    Update prepare_gist_metrics.sh

--- .github/prepare_gist_metrics.sh
+++ .github/prepare_gist_metrics.sh
@@ -59,7 +59,7 @@ else
      m=$(curl -L \
       -X POST \
       -H "Accept: application/vnd.github+json" \
-      -H "Authorization: Bearer <YOUR-TOKEN>" \
+      -H "Authorization: Bearer $YOUR_TOKEN" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       https://api.github.com/gists \
       -d '{"description":"Example of a gist","public":false,"files":{"README.md":{"content":"Hello World"}}}')
