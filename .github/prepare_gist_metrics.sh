4470a4cb4a73860f34aeca5819817bd3b7b42199
Vadim Dor <V.Dorez@mail.ru>
2024-03-18 23:38:28 +0300

    Update prepare_gist_metrics.sh

--- .github/prepare_gist_metrics.sh
+++ .github/prepare_gist_metrics.sh
@@ -7,6 +7,7 @@ do
         n) name=${OPTARG};;
         u) urlname=${OPTARG};;
         d) description=${OPTARG};;
+        g) gist_magic_desc=${OPTARG};;
     esac
 done
 
@@ -25,13 +26,6 @@ echo "YOUR_TOKEN: $YOUR_TOKEN";
 
 echo "Preparing gist..."
 
-#echo "$YOUR_TOKEN" | gh auth login --with-token
-#gh api repos/${{ GITHUB.REPOSITORY }}/issues  --jq '.[].title'
-#gh api \
-#  -H "Accept: application/vnd.github+json" \
-#  -H "X-GitHub-Api-Version: 2022-11-28" \
-#  /gists  --jq='.[].description'
-
 msg="test highlight1"
 if [[ $(curl -L \
   -H "Accept: application/vnd.github+json" \
@@ -41,46 +35,21 @@ if [[ $(curl -L \
     echo "Found GIST with description '$msg'. Nothing to do."
 else
     echo "GIST for holding of generated github statistic images not found. Trying to create.."
-   
-    json="'{\"description\":\"$gist_magic_desc\",\"public\":true,\"files\":{\"README.md\":{\"content\":\"hahaha\"}}}'"
-    json=$(echo '{"description":"ddd","public":true,"files":{"README.md":{"content":"hahaha"}}}')
-    echo $json
-    echo "'$json'"
-   #  -d '{"description":"edede","public":true,"files":{"README.md":{"content":"Tdee"}}}'
-   #     '{"description":"tdddd","public":true,"files":{"README.md":{"content":"cddc"}}}'
-   #  -d '{"description":"fffff","public":true,"files":{"README.md":{"content":"ffff"}}}'
-    #curl -L --fail   \
-    #  -H "Accept: application/vnd.github+json" \
-    #  -H "Authorization: Bearer $YOUR_TOKEN" \
-    #  -H "X-GitHub-Api-Version: 2022-11-28" \
-    #  https://api.github.com/gist \
-    #  -d '{"description":"ddd","public":true,"files":{"README.md":{"content":"hahaha"}}}'   
-     echo "--------------------" 
-     msg='haha haha1'
-     echo $msg
-     m=$(curl -L --fail  \
+    m=$(curl -L --fail  \
       -X POST \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer $YOUR_TOKEN" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       https://api.github.com/gists \
-      -d '{"description":"'"$gist_magic_desc"'","public":false,"files":{"README.md":{"content":"'"$gist_magic_desc"'"}}}}')
-      #-d "'$json'")
+      -d '{"description":"'"$gist_magic_desc"'","public":false,"files":{"README.md":{"content":"'"$gist_magic_desc"'"}}}')
 
-      #m=$(curl -L --fail   \
-      #-X POST \
-      #-H "Accept: application/vnd.github+json" \
-      #-H "Authorization: Bearer $YOUR_TOKEN" \
-      #-H "X-GitHub-Api-Version: 2022-11-28" \
-      #https://api.github.com/gist \
-      #-d '{"description":"ddd","public":true,"files":{"README.md":{"content":"hahaha"}}}' 2>&1)
     if [ $? -ne 0 ] ; then
        echo "Could not create GIST. Create first token GIST_SECRET with appropriate permitions. Error executing CURL: $m"
        exit 4
     else   
        echo "Configured new GIST as a container for metrics"  
+       echo "Output from CURL: $m"
+       echo $m | jq '.url'
     fi     
-     echo "Output from CURL: $m"
-     echo $m | jq '.url'
 fi
   
